1144eb4ccfdc3bff2fb8f0956fbb1ccb1564663d
Florian Rau <bimac@users.noreply.github.com>
2023-05-26 20:09:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EEPstore
+ EEPROM data storage with default values and CRC
---
+++ include/EEPstore.h
@@ -0,0 +1,65 @@
+/*
+This file is part of EEPstore.
+Copyright (C) 2023 Florian Rau.
+
+EEPstore is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version.
+
+EEPstore is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with EEPstore. If not, see <https://www.gnu.org/licenses/>.
+
+_______________________________________________________________________________
+
+
+REVISION HISTORY
+
+version 1.0.0   initial release (thank you: Florian Uekermann)
+version 1.0.1   default address: 0
+version 1.0.2   reorganize, relicense & publish as library
+
+_______________________________________________________________________________
+*/
+
+#pragma once
+
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <util/crc16.h>
+
+template <class T> class EEPstore {
+
+  EEPstore(const T &dataRef) : data(dataRef), crc(calcCRC()) {}
+
+  uint16_t calcCRC() {
+    uint16_t out = 0;
+    const uint8_t *bytes = (uint8_t *)&data;
+    for (size_t i = 0; i < sizeof(T); i++) {
+      out = _crc16_update(out, bytes[i]);
+    }
+    return out;
+  }
+
+  const T data;
+  const uint16_t crc;
+
+public:
+  static void get(T &dataRef, const uint16_t address = 0) {
+    EEPstore<T> storage(dataRef);
+    EEPROM.get(address, storage);
+    if (storage.crc == storage.calcCRC()) {
+      dataRef = storage.data;
+    } else {
+      EEPstore<T>::set(dataRef, address);
+    }
+  }
+
+  static void set(const T &dataRef, const uint16_t address = 0) {
+    EEPstore<T> storage(dataRef);
+    EEPROM.put(address, storage);
+  }
+};
